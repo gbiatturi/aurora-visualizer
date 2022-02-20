b9342f6866977dc50c32f0938aec260888f94fa5
Guillermo Biatturi <gbiatturi@gmail.com>
2022-02-20 12:57:39 -0300

    Add txs table

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -3,7 +3,10 @@ import {
     Card,
     Grid,
     Container,
-    Icon
+    Icon,
+    Table,
+    Pagination,
+    Label
 } from "semantic-ui-react";
 
 import { useEthers } from '@usedapp/core'
@@ -12,32 +15,95 @@ import { formatEther } from '@ethersproject/units'
 import axios from 'axios'
 
 const Overview = () => {
-    const { account } = useEthers()
-    const [balance, setBalance] = useState()
+    // const { account } = useEthers()
+    const account = "0x355c1cf73c2c1d0193b0dac92863d5f254a3e83d" // Test account
+
+    const [balance, setBalance] = useState({ pending: true })
+    const [blockInfo, setBlockInfo] = useState({ pending: true })
+    const [pagination, setPagination] = useState({
+        pending: true,
+        txs: [],
+        txsDisplay: [],
+        begin: 0,
+        end: 5,
+        activePage: 1
+    })
 
     const KEY = "FM9FQH85BJVI1B6JAQBCHK46FNHXQGGWIA"
 
     const getBalance = () => {
-        console.log(account)
-        axios
+        return axios
             .get(`https://api-testnet.aurorascan.dev/api?module=account&action=balance&address=${account}&tag=latest&apikey=${KEY}`)
-            .then(response => {
-                console.log(response)
-                setBalance(response.data.result);
-            })
+
     }
 
-    useEffect(getBalance, [account])
+    const getBlockInfo = () => {
+        return axios
+            .get(`https://api-testnet.aurorascan.dev/api?module=proxy&action=eth_blockNumber&apikey=${KEY}`)
+    }
+
+    const getTransactions = () => {
+        return axios
+            .get(`https://api.aurorascan.dev/api?module=account&action=txlist&address=${account}&startblock=1&endblock=${blockInfo.blockNo}&sort=desc&apikey=${KEY}`)
+    }
+
+    const changePage = (event, data) => {
+        setPagination({
+            ...pagination,
+            activePage: data.activePage,
+            begin: data.activePage * 5 - 5,
+            end: data.activePage * 5,
+            txsDisplay: pagination.txs.slice(data.activePage * 5 - 5, data.activePage * 5)
+        })
+    }
+
+    useEffect(() => {
+        if (account) {
+
+            getBalance().then((result) => {
+                setBalance({ pending: false, value: result.data.result.length != 0 ? parseInt(result.data.result) : 0 });
+            });
+
+            getBlockInfo().then((result) => {
+                setBlockInfo({
+                    pending: false,
+                    latestBlock: parseInt(result.data.result),
+                    blockNo: result.data.result, // Saved in HEX
+                });
+            });
+
+            getTransactions().then((result) => {
+                setPagination({
+                    ...pagination,
+                    pending: false,
+                    txs: result.data.result,
+                    txsDisplay: result.data.result.slice(pagination.begin, pagination.end),
+                });
+            });
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log("pagination", pagination)
+    }, [pagination])
+
+    useEffect(() => {
+        console.log("blockInfo", blockInfo)
+    }, [blockInfo])
+
+    useEffect(() => {
+        console.log("balance", balance)
+    }, [balance])
 
     return (
         <Container>
             <Grid>
-                <Grid.Row columns={2} style={{ margin: "margin: 25px 0 0 0" }}>
+                <Grid.Row columns={2} style={{ margin: "25px 0 0 0" }}>
                     <Grid.Column width={12}>
                         <Card fluid>
                             <Card.Content>
                                 <Card.Header>
-                                   Account
+                                    Account
                                 </Card.Header>
                                 <Card.Description textAlign="left">
                                     {account}
@@ -53,12 +119,76 @@ const Overview = () => {
                                     <Icon name="ethereum"></Icon> Current balance
                                 </Card.Header>
                                 <Card.Description textAlign="left">
-                                    {balance} ETH
+                                    {!balance.pending && balance.value} ETH
                                 </Card.Description>
                             </Card.Content>
                         </Card>
                     </Grid.Column>
                 </Grid.Row>
+            </Grid>
+
+            <Grid>
+                <Grid.Row>
+                    <Grid.Column>
+                        <Table fixed>
+                            <Table.Header>
+                                <Table.Row>
+                                    <Table.Cell>
+                                        <h3>Latest Transactions</h3>
+                                    </Table.Cell>
+                                </Table.Row>
+                            </Table.Header>
+
+                            <Table.Header>
+                                <Table.Row>
+                                    <Table.Cell>
+                                        <h4>Hash Txn</h4>
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        <h4>From & To</h4>
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        <h4>Value</h4>
+                                    </Table.Cell>
+                                </Table.Row>
+                            </Table.Header>
+
+                            <Table.Body>
+                                {
+                                    !pagination.pending && pagination.txsDisplay.map((tx, i) => {
+                                        return (
+                                            <Table.Row key={i}>
+                                                <Table.Cell singleLine >
+                                                    <Label color="black" style={{ margin: "0 5px 0 0" }}>Tx</Label>{tx.hash}
+                                                </Table.Cell>
+                                                <Table.Cell >
+                                                    <b>From</b> {tx.from}
+                                                    <br></br>
+                                                    <b>To</b> {tx.to}
+                                                </Table.Cell>
+                                                <Table.Cell >
+                                                    {" "}
+                                                    <Label color="black">ETH</Label> {parseInt(tx.value) / 10 ** 18}
+                                                </Table.Cell>
+                                            </Table.Row>
+                                        )
+                                    })
+                                }
+                            </Table.Body>
+                        </Table>
+
+                        {
+                            !pagination.pending &&
+                            <Pagination
+                                defaultActivePage={1}
+                                totalPages={Math.ceil(pagination.txs.length / 5)}
+                                onPageChange={changePage}
+                            />
+                        }
+
+                    </Grid.Column>
+                </Grid.Row>
+
             </Grid>
         </Container>
     );
